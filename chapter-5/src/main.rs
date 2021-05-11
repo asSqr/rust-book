657d2481ddfe36d49fa8d481c8ba10d210ace0c3
asSqr <ryo.yossy@live.jp>
2021-05-11 21:48:03 +0900

    fix: MyError 追加

--- chapter-5/src/main.rs
+++ chapter-5/src/main.rs
@@ -1,7 +1,13 @@
 use actix_web::{get, App, HttpResponse, HttpServer};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MyError {}
+
+impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
+async fn index() -> Result<HttpResponse, MyError> {
   let response_body = "Hello World!";
   Ok(HttpResponse::Ok().body(response_body))
 }
