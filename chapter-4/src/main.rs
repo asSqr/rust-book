2f3af12299606080b457c4b0cf23c7a6f816deaa
Ysmr-Ry <ryo.yossy@live.jp>
2022-04-29 11:21:58 +0900

    fix: chapter-4 とりあえず動いた

--- chapter-4/src/main.rs
+++ chapter-4/src/main.rs
@@ -1,32 +1,123 @@
-use clap::{Command, Arg};
+use clap::Parser;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "As_sqr",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    // Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    // Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 
 fn main() {
-    let matches = Command::new("My RPN program")
-        .version("1.0.0")
-        .author("As_sqr")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::new("formula_file")
-                .help("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::new("verbose")
-                .help("Sets the level of verbosity")
-                .short('v')
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
+    let opts = Opts::parse();
 
-    match matches.value_of("formula_file") {
-        Some(file) => println!("Filed specified: {}", file),
-        None => println!("No file specified."),
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        run(reader, opts.verbose);
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
+    }
+}
+
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
     }
 
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified: {}", verbose);
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    pub fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+
+                stack.push(res);
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
+}
+
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let answer = calc.eval(&line);
+
+        println!("{}", answer);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        let calc = RpnCalculator::new(false);
+        assert_eq!(calc.eval("5"), 5);
+        assert_eq!(calc.eval("50"), 50);
+        assert_eq!(calc.eval("-50"), -50);
+
+        assert_eq!(calc.eval(" 2 3 +"), 5);
+        assert_eq!(calc.eval(" 2 3 *"), 6);
+        assert_eq!(calc.eval(" 2 3 -"), -1);
+        assert_eq!(calc.eval(" 2 3 /"), 0);
+        assert_eq!(calc.eval(" 2 3 %"), 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_ng() {
+        let calc = RpnCalculator::new(false);
+        calc.eval("1 1 ^");
+    }
 }
